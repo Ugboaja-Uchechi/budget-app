9f64e8aff013d08d4505430af9ea47176f527cd4
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-22 15:05:31 +0100

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
          :trackable, :confirmable
   has_many :groups, class_name: 'Group', foreign_key: 'author_id'
   has_many :entities, class_name: 'Entity', foreign_key: 'author_id'
+
+  validates :name, presence: true
 end
