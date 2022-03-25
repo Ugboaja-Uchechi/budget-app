0a203b831a40a6f5165a2821fef0c3c12925abf6
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-25 18:11:36 +0100

    Comment code

--- db/schema.rb
+++ db/schema.rb
@@ -55,11 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_115938) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
