7c20386acca7fda5d3eb519e933e05720c23617f
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-26 21:24:08 +0100

    Add optional requirement

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_115938) do
 
   create_table "entities", force: :cascade do |t|
     t.string "name"
-    t.string "amount"
+    t.float "amount"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
